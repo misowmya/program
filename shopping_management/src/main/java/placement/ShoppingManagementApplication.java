b1da48c5b78b9a2f3f383cf8d7c4822e17583c97
pavi5 <pavi5@LAPTOP-B44HQDM0>
2023-02-19 21:40:41 +0530

    first commit

---
+++ shopping_management/src/main/java/placement/ShoppingManagementApplication.java
@@ -0,0 +1,13 @@
+package placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingManagementApplication.class, args);
+	}
+
+}
